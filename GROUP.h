421d4ab46cdc30cd509aa84200d2ad951a9ad36d
taisyavarchenko <55127370+taisyavarchenko@users.noreply.github.com>
2019-12-16 20:40:07 +0300

    Изменен текст файла readme

---
+++ GROUP.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "STUDENT.h"
+
+class GROUP {
+	STUDENT* students; // студенты
+	int capacity; // ёмкость
+	int size; // размер
+public:
+	GROUP(); // конструктор по умолчанию
+	GROUP(const GROUP& group); // конструктор копирования
+	GROUP(STUDENT* students, int size); // конструктор с параметрами
+
+	GROUP& operator=(const GROUP& group); // оператор присваивания
+	void Sort(); // сортировка по номеру группы
+	void AddStudent(); // добавление студента
+	void RemoveStudent(); // удаление студенту
+	void PrintByKey(); // вывод тех, у кого балл выше 4
+
+	~GROUP(); // деструктор (освобождение памяти)
+
+	friend std::ostream& operator<<(std::ostream& os, const GROUP& group); // оператор вывода в поток
+};
---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "STUDENT.h"
+#include "GROUP.h"
+
+int main() {
+	GROUP group;
+	int item; // выбираемый пункт меню
+
+	do {
+		system("cls"); // очищаем экран
+		// выводим меню
+		cout << "What do you want to do?" << endl;
+		cout << "1. Add student" << endl;
+		cout << "2. Remove student" << endl;
+		cout << "3. Print group" << endl;
+		cout << "4. Print students with average score more then 4" << endl;
+		cout << "5. Quit" << endl;
+		cout << ">";
+		cin >> item; // считываем пункт меню
+		cin.ignore();
+
+		try {
+			// обрабатываем некорректный ввод
+			if (item < 1 || item > 5) {
+				throw string("Incorrect item.");
+			}
+
+			// в зависимости от выбранного пункта выполняем действие
+			switch (item) {
+			case 1:
+				group.AddStudent();
+				break;
+
+			case 2:
+				group.RemoveStudent();
+				break;
+
+			case 3:
+				cout << group;
+				break;
+
+			case 4:
+				group.PrintByKey();
+				break;
+			}
+		}
+		// ловим исключения
+		catch (string s) {
+			cout << s << endl;
+		}
+
+		if (item != 5) {
+			system("pause"); // задерживаем экран
+		}
+
+	} while (item != 5); // повторяем, пока не решим выйти
+
+
+	return 0;
+}
